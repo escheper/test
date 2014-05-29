31f82909a895957f8ae0dd106cc6dff2e099a852
escheper <eddy.scheper@aris.nl>
2014-05-29 22:56:28 +0200

    1e .net commit

--- source_aris.js
+++ source_aris.js
@@ -1,11 +1,11 @@
-var s = "zxczx";
+  var s = "zxczx";
 
-var t = "aanpassing 20140527"
+  var t = "aanpassing 20140527"
 
-var t = "aanpassing 20140527 4a2"
+  var t = "aanpassing 20140527 4a2"
 
-var t = "aanpassing 20140528 master"
+  var t = "aanpassing 20140528 master"
 
-var t = "aanpassing 201405xx 2 master"
+  var t = "aanpassing 201405xx 2 master"
 
-var t = "aanpassing 20140529 master"
+  var t = "aanpassing 20140529 master"
